30ba1fa4c718b86ebd8b02dc871f9ba9d33d7327
csaszi <csaszika4@gmail.com>
2017-09-16 15:17:15 +0200

    remove unused effect + tsconfig => es6

--- src/app/effects/player-effects.ts
+++ src/app/effects/player-effects.ts
@@ -16,14 +16,6 @@ export class PlayerEffects {
       {id: 124, name: 'Tom', speed: 77, agility: 56, power: 65, moral: 87, luck: 77},
       {id: 125, name: 'Michael', speed: 81, agility: 67, power: 78, moral: 76, luck: 88}]));
 
-  // @Effect()
-  // loadFeatureEffect$ = this._actions$
-  //   .ofType(LOAD_FEATURE)
-  //   .map(toPayload)
-  //   .delay(1000)
-  //   .map((id) => new FeatureLoaded(
-  //     {id, name: 'First ' + id, description: 'Desc First ' + id, enabled: true}));
-
   constructor(private _actions$: Actions) {
   }
 }
